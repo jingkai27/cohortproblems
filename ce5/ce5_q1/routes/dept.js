a18974d4b3478d89a62bbd637b9c61ab91345463
jingkai27 <jingkai.t27@gmail.com>
2024-06-13 13:31:33 +0800

    finish ce1
    yippee

--- ce5/ce5_q1/routes/dept.js
+++ ce5/ce5_q1/routes/dept.js
@@ -4,23 +4,28 @@ const staffmodel = require('../models/staff.js');
 var router = express.Router();
 
 
-router.get('/add/:code', async function(req, res, next) {
-    
-    res.send(`TODO`); // TODO: Fixme
+router.get('/add/:code', async function (req, res, next) {
+    const msg = req.params.code;
+    const dept = new deptmodel.Dept(msg);
+    await deptmodel.insertMany([dept]);
+    res.send(`${JSON.stringify(dept)}`); // Send the new department as the response
 });
-
-
 
 /* GET dept listing. */
-
-router.get('/all/', async function(req, res, next) {
-    res.send(`TODO`); // TODO: Fixme
+router.get('/all/', async function (req, res, next) {
+    const depts = await deptmodel.all();
+    res.send(`${JSON.stringify(depts)}`); // TODO: Fixme
 });
 
 
-router.get('/all/withstaff/', async function(req, res, next) {
-    res.send(`TODO`); // TODO: Fixme
+router.get('/all/withstaff/', async function (req, res, next) {
+    const depts = await deptmodel.all();
+    for (let i = 0; i < depts.length; i++) {
+        const staffs = await staffmodel.find({ dept: depts[i].code });
+        depts[i].staffs = staffs;
+    }
+    res.send(`${JSON.stringify(depts)}`); // Send the list of depts with staff as the response
 })
-
+// desired output: [{ "code": "hr", "staffs": [{ "id": "1", "name": "aaron", "dept": "hr" }] }]
 
 module.exports = router;
--- ce5/ce5_q1/routes/staff.js
+++ ce5/ce5_q1/routes/staff.js
@@ -5,14 +5,18 @@ var router = express.Router();
 
 
 /* insert a staff, should have used POST instead of GET */
-router.get('/add/:id/:name/:code', async function(req, res, next) {
-    res.send(`TODO`); // TODO: Fixme
+router.get('/add/:id/:name/:code', async function (req, res, next) {
+    const msg = req.params;
+    const staff = new staffmodel.Staff(msg.id, msg.name, msg.code);
+    await staffmodel.insertMany([staff]);
+    res.send(`${JSON.stringify(staff)}`); // Send the new staff as the response
 });
 
 /* GET staff listing. */
 
-router.get('/all/', async function(req, res, next) {
-    res.send(`TODO`); // TODO: Fixme
+router.get('/all/', async function (req, res, next) {
+    const staff = await staffmodel.all();
+    res.send(`${JSON.stringify(staff)}`); // Send the list of staff as the response
 });
 
 
